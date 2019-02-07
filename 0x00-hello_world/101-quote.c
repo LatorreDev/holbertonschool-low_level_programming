94fca89f7c24dfac53ad63e72d10369e155c262a
David Latorre <david@latorredev.com>
2019-02-06 19:53:40 -0500

    awrite a program to print message without the use of print or puts function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -8,8 +7,6 @@
 int main(void)
 {
 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-
 }

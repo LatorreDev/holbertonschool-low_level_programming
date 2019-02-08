33aa4276d6c0270fa62d315a5b72830c1169e40b
David Latorre <david@latorredev.com>
2019-02-07 22:27:27 -0500

    00 al 99 with ascii

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -12,16 +12,16 @@ int j;
 
 for (i = 48 ; i <= 57 ; i++)
 {
-    for (j = 48 ; j <= 57 ; j++)
-    {
-        putchar (i);
-        putchar (j);
-        if (i != 57 || j!=57)
-        {
-        putchar (',');
-        putchar (' ');
-        }
-    }
+for (j = 48 ; j <= 57 ; j++)
+{
+putchar (i);
+putchar (j);
+if (i != 57 || j != 57)
+{
+putchar (',');
+putchar (' ');
+}
+}
 }
 putchar('\n');
 return (0);

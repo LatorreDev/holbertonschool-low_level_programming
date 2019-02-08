573d90fa04fe22698b2d335d8ec5422b7755dd0b
David Latorre <david@latorredev.com>
2019-02-08 16:04:14 -0500

    add first advanced exercise

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *   main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+    {
+        char i, j;
+
+            for (i='0' ; i<'9' ; i++)
+            {
+                    for (j='0'; j<='9' ; j++)
+                    {
+                        putchar(i);
+                        putchar(j);
+                        if (i != '8' || j != '9')
+                            {
+                        putchar(',');
+                        putchar(' ');
+                            }
+                    }
+            }
+            putchar('\n');
+            return (0);
+    }
+
+

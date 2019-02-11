2f3da21229b55e0a977421d82cf0c79584eb41dd
David Latorre <david@latorredev.com>
2019-02-10 23:25:09 -0500

    add betty style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,29 +6,29 @@
  */
 
 int main(void)
-    {
-        int i, j, k;
+{
+int i, j, k;
 
-            for (i = 48 ; i <= 57 ; i++)
-                {
-                    for (j = 48 ; j <= 57 ; j++)
-                        {
-                            for (k = 48 ; k <= 57 ; k++)
-                                {
-                                    if (i < j && j < k)
-                                        {
-                                            putchar(i);
-                                            putchar(j);
-                                            putchar(k);
-                                            if (i != 55 || j != 56 || k != 57)
-                                                {
-                                                    putchar(',');
-                                                    putchar(' ');
-                                                }
-                                        }
-                                }
-                        }
-                }
-            putchar('\n');
-            return (0);
-    }
+for (i = 48 ; i <= 57 ; i++)
+{
+for (j = 48 ; j <= 57 ; j++)
+{
+for (k = 48 ; k <= 57 ; k++)
+{
+if (i < j && j < k)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != 55 || j != 56 || k != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

ee97cd17eea2e57816fbb9e23fe0783891e4dced
David Latorre <david@latorredev.com>
2019-02-12 17:36:42 -0500

    add betty style

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,15 @@
 
 int print_last_digit(int n)
 {
-        n = n%10;
-        if (n > 0)
-        {
-        _putchar(n + '0');
-        }
-
-        else {
-            n = n * -1;
-        _putchar(n + '0');
-            }
-        return(n);
+n = n % 10;
+if (n > 0)
+{
+_putchar(n + '0');
+}
+else
+{
+n = n * -1;
+_putchar(n + '0');
+}
+return (n);
 }

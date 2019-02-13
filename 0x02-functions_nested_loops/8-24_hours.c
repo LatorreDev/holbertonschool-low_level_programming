5d8f9465991972775f970dca5a66a9ae3600bb78
David Latorre <david@latorredev.com>
2019-02-12 19:42:40 -0500

    add betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,8 +2,8 @@
 
 /**
  *  jack_bauer- entry point
- *  @n: the character to print
- *  Return: n character
+ *  @i: the character to print
+ *  @j: the character to print
  *
 */
 
@@ -11,19 +11,18 @@ void jack_bauer(void)
 {
 int i,j;
 
-
-        for (i = 00 ; i <= 23 ; i++)
-        {
-            for (j = 00 ; j <= 59 ; j++)
-                {
-                        _putchar(i/10 + '0');
-                        _putchar(i%10 + '0');
-                        _putchar(':');
-                        _putchar(j/10 + '0');
-                        _putchar(j%10 + '0');
-                        _putchar('\n');
-                }
-        }
+for (i = 00 ; i <= 23 ; i++)
+{
+for (j = 00 ; j <= 59 ; j++)
+{
+_putchar(i/10 + '0');
+_putchar(i%10 + '0');
+_putchar(':');
+_putchar(j/10 + '0');
+_putchar(j%10 + '0');
+_putchar('\n');
+}
+}
 }
 
 

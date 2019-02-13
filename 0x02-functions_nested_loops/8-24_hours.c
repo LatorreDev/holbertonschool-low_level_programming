e9ccb25a72144ce5dbd27901996e74f776ef62f5
David Latorre <david@latorredev.com>
2019-02-12 19:43:50 -0500

    add betty style

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ for (i = 00 ; i <= 23 ; i++)
 {
 for (j = 00 ; j <= 59 ; j++)
 {
-_putchar(i/10 + '0');
-_putchar(i%10 + '0');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
 _putchar(':');
-_putchar(j/10 + '0');
-_putchar(j%10 + '0');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 _putchar('\n');
 }
 }

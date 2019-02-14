5bb389dab86c41872d983912869b1ca2e1f90a7d
David Latorre <david@latorredev.com>
2019-02-14 17:07:18 -0500

    print numbers from 0 to 14

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,20 @@
 
 /**
  * more_numbers - entry point
- *
  */
 
 void more_numbers(void)
 {
-    int i,j;
-    for (i = 0 ; i < 10 ; i++)
-    {
-        for (j = 0 ; j < 15 ; j++)
-        {
-            if (j > 10){
-                _putchar( j / 10 + '0');
-            }
-            _putchar( j % 10 + '0' );
-        }
-        _putchar('\n');
-    }
+int i, j;
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 0 ; j <= 14 ; j++)
+{
+if (j > 10)
+_putchar(j / 10 + '0');
 
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
 }

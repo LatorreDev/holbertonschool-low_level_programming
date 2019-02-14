bd4f810055891099ac05be7bd3e3ca49b88da8e5
David Latorre <david@latorredev.com>
2019-02-14 14:35:22 -0500

    add program to multipl two variables

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - entry point
+ *
+ * @a: first value
+ * @a: second value
+ *
+ * Return: multiplication between a and b
+ *
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}

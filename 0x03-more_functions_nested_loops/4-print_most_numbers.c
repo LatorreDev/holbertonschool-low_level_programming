0a020d30cd899f92782ac164a0cf55f52eaa8a82
David Latorre <david@latorredev.com>
2019-02-14 15:16:53 -0500

    add funtion to print numbers from 0 until 9 except 2 and 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ */
+
+void print_most_numbers(void)
+{
+int a;
+for (a = 48 ; a <= 57 ; a++)
+{
+if (a != 50 && a != 52)
+_putchar(a);
+}
+_putchar('\n');
+}

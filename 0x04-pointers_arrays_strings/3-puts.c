3f94223b37a8bf862ed641b8ff2e8daef551348e
David Latorre <david@latorredev.com>
2019-02-20 22:00:46 -0500

    add program to priint a char array with putchar

---
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* _puts - entry point
+* @str: string array
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; i[str] != '\0' ; i++)
+		_putchar (str[i]);
+
+	_putchar ('\n');
+
+}

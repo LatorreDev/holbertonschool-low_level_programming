bbcbaab623dab14c2e1f363e2c8dbfcc5ed87426
David Latorre <david@latorredev.com>
2019-02-24 00:09:42 -0500

    add exercise 9

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: always success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		if (i % 5 == 0)
+			printf("Buzz ");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%i ", i);
+	}
+	putchar(10);
+	return (0);
+
+}

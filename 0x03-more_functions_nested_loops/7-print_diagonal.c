4149f72d473c087d69bb0e99675a61ee4f2c150d
David Latorre <david@latorredev.com>
2019-02-23 23:27:52 -0500

    add betty style

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_line - entry point
+ * print_diagonal - entry point
  * @n: entry variable
 */
 
@@ -13,18 +13,18 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar (10);
 
-	for (i = 0 ; i < n ; i++)
-		{
-			for(j = 0 ; j < n ; j++)
+		for (i = 0 ; i < n ; i++)
 			{
-				if(i==j)
+			for (j = 0 ; j < n ; j++)
+				{
+				if (i == j)
 					{
 					_putchar(92);
 					break;
 					}
-				else
+					else
 					_putchar(32);
+				}
+			_putchar(10);
 			}
-		_putchar(10);
-		}
 	}

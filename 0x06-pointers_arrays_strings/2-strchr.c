a456da4e7799b3435ab750892067bd4462bc563e
David Latorre <david@latorredev.com>
2019-02-25 17:10:18 -0500

    add exercise 2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _strchr - entry point
+* Return: array
+* @dest: array
+* @src: value
+* @n: int
+*/
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0 ; s[i] ; i++)
+	{
+		if (s[i] == c)
+			return(&s[i]);
+	}
+return (0);
+}

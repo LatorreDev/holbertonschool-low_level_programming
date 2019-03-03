565924c2714ddf1545da281301eb3aca7ad53eac
David Latorre <david@latorredev.com>
2019-03-03 17:26:02 -0500

    add basic structure to file

---
+++ 0x08-static_libraries/1-strncat.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _strncat - entry point
+* @dest: entry array
+* @src: entry array
+* @n: entry parameter
+* Return: always success
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
+

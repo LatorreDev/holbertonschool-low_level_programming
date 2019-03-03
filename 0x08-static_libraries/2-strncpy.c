e91fdeac30447f5fd7908038b9bb35f195705f37
David Latorre <david@latorredev.com>
2019-03-03 16:32:29 -0500

    add betty style

--- 0x08-static_libraries/2-strncpy.c
+++ 0x08-static_libraries/2-strncpy.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
 /**
 * _strncpy - entrypoint
+* @dest: entry array
+* @src: entry array
+* @n: entry variable
+* Return: always success
 */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 }
- 

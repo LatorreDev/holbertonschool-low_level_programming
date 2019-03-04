5ac4ba08abff3cdc0edcbc7d3df9c832a3084244
David Latorre <david@latorredev.com>
2019-03-03 19:55:57 -0500

    add basic structuture to file

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* myname - entry point
+* @argc: count arguments
+* @argv: array arguments
+* Return: always sucess
+*/
+
+int myname(int argc, char *argv[])
+{
+	return (0);
+}

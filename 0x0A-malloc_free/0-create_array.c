b36e312b27d8fe85f7068cec587474eac688b128
David Latorre <david@latorredev.com>
2019-03-07 14:54:42 -0500

    add basic structure

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 
 /**
-*
-*
+* create_array - entry point
+* @size: size of string
+* @c: entry character
+* Return: always sucess!
 */
 
+char *create_array(unsigned int size, char c)
+{
 
+}

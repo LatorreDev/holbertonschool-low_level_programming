0a7f34ee6bc12b2e50ade60387b90fe293877c7b
David Latorre <david@latorredev.com>
2019-03-08 10:03:58 -0500

    add betty style

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* string_nconcat - entry point
+* @s1: entry string
+* @s2: entry string
+* @n: entry int
+* Return: Always sucesss
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+
+}

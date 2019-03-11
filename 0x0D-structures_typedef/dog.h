543be4bbb208b8caa3118db7198b79b1e1e4aa1c
David Latorre <david@latorredev.com>
2019-03-11 18:25:04 -0500

    woof betty woof

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,8 +1,15 @@
 #ifndef doggy
 #define doggy
+/**
+* struct dog - woof woof
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*/
+
 struct dog
 {
-	char *name; 
+	char *name;
 	float age;
 	char *owner;
 };

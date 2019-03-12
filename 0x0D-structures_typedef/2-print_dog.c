7ad5b89232f36ef752050a4cc0764ac3d6c811f4
David Latorre <david@latorredev.com>
2019-03-11 20:06:25 -0500

    orgaize code

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,18 +14,17 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)\n");
 		}
-
-		if (d)
-		{
-			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
+		else
 			printf("Owner: %s\n", d->owner);
-		}
 
 	}
 }

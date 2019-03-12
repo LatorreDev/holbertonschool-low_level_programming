fd9471ac9e22bbe496da3f1361d5871b19cf0e27
David Latorre <david@latorredev.com>
2019-03-11 19:50:42 -0500

    add dog card

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+
+	}
+
+	if (d->name == 0)
+	{
+		printf("Name: (nil)");
+	}
+
+	if (d->age == 0)
+	{
+		printf("Age: (nil)");
+	}
+	
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)");
+	}
+
+	if(d)
+	{
+		printf ("Name: %s\n", d->name);
+		printf ("Age: %f\n", d->age);
+		printf ("Owner: %s\n", d->owner);
+	}
+
+
+}

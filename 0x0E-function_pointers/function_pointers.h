bb7f42c86637094f0fdf49f5c9e35810442b559a
David Latorre <david@latorredev.com>
2019-03-13 15:42:32 -0500

    redo changes to function pointers

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef PRINT_NAME
 #define PRINT_NAME
-#define size_t int
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

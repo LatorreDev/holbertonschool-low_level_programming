803eb1b3b068f35e16230b877d5b760521474241
David Latorre <david@latorredev.com>
2019-03-13 15:25:11 -0500

    add define for size_t

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef PRINT_NAME
 #define PRINT_NAME
+#define size_t int
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

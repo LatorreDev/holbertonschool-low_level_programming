b1232be8c5cc78cffa0a1884f847aad53a5eeedf
David Latorre <david@latorredev.com>
2019-03-15 17:20:19 -0500

    add exercise 2

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -3,27 +3,32 @@
 #include <stdio.h>
 
 /**
-* print_numbers - entry point
-* @separator: entry string
-* @n: entry argument
+*print_strings - entry point
+*@separator: entry string
+*@n: entry argument
 */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	if (separator != NULL)
 	{
 		va_list my_list;
 		unsigned int counter;
+		char *my_little_var;
 
 		va_start(my_list, n);
 
 		for (counter = 0 ; counter < n; counter++)
 		{
-			printf("%i", va_arg(my_list, int));
+			my_little_var = va_arg(my_list, char *);
+			if (my_little_var == NULL)
+				printf("(nil)");
+			else
+				printf("%s", my_little_var);
 			if (counter < n - 1)
 				printf("%s", separator);
 		}
 		printf("\n");
 		va_end(my_list);
 	}
-}
+

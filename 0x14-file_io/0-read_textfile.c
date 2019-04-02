aab7311b4f1f1c1df9ea0c41092ff4bef20b686e
David Latorre <david@latorredev.com>
2019-04-02 18:40:52 -0500

    [Feature] fix return from -1 to 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	my_file = open(filename, O_RDONLY);
 	if (my_file == -1)
-		return (-1);
+		return (0);
 	my_buffer = malloc(sizeof(char) * letters);
 	if (my_buffer == NULL)
 	{

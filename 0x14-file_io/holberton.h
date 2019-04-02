0a2949867f4f7556a1f58b336cc5f09809635490
David Latorre <david@latorredev.com>
2019-04-02 14:18:35 -0500

    add holberton header file

---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);

0c752e2e982eb562d5ebe77750e1abf749c32bd6
David Latorre <david@latorredev.com>
2019-04-02 16:06:49 -0500

    [Feature] add guards

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,9 +1,14 @@
+#ifndef HOLBERTON
+#define HOLBERTON
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif /* HOLBERTON */

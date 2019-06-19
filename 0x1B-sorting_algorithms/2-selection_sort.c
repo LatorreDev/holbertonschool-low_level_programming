22726e3f1f366ec8061bf9bf6f2dc97230529ea9
nildiert <niljordan23@gmail.com>
2019-06-19 23:30:13 +0000

    [FIX] Added swap fucntion in file 2

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,5 +1,20 @@
 #include "sort.h"
 /**
+* swap - swap for the bubble sort
+*
+* @first: entry int
+* @second: entry int
+*/
+
+void swap(int *first, int *second)
+{
+	int temp = 0;
+
+	temp = *first;
+	*first = *second;
+	*second = temp;
+}
+/**
  * selection_sort - Selection sort algoritm
  *
  * @array: Size of the array
@@ -8,18 +23,25 @@
 void selection_sort(int *array, size_t size)
 {
 	unsigned int i, j;
-	int min_num_id;
+	int min_num_id, flag = 0;
 
 	if (array != NULL && size >= 2)
 	{
 		for (i = 0; i < size - 1; i++)
 		{
 			min_num_id = i;
-			for(j = i + 1; j < size - 1; j++)
-				if (array[j] > array[min_num_id])
+			for(j = i + 1; j < size; j++)
+				if (array[j] < array[min_num_id])
+				{
 					min_num_id = j;
-			swap(&array[min_num_id], &array[i]);
-			print_array(array, size);
+					flag = 1;
+				}
+			if (flag)
+			{
+				swap(&array[min_num_id], &array[i]);
+				print_array(array, size);
+				flag = 0;
+			}
 		}
 	}
 	else
